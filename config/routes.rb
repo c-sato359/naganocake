39f42eb4dd9225e6ea2ba24000c7d6782a62e1f0
c-sato359 <kawaritudukeru658@gmail.com>
2022-12-22 04:33:08 +0000

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #devise_for :admins
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'}
+  
+  #resources :cart, only: [:show]
+  #resources :charge, only: [:create]
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"}
+ 
+  root "items#index"
+ # resources :items, only: [:show] do
+    #scope module: :items do
+     # resources :add_to_carts, only: [:create]
+      #resources :delete_in_carts, only: [:create]
+    #end
+       #resources :items, only: [:new, :create]
+  #end# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
