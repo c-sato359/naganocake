6ab52aae1ef7bd22f95119dcacd27fe6ad788b05
EC2 Default User <ec2-user@ip-172-31-16-183.ap-northeast-1.compute.internal>
2023-02-26 10:51:48 +0000

    finish naganocake

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
   has_many :items, through: :order_details
 
 
-  enum payment_method: {  "クレジットカード": 0, "銀行振込": 1 }
+  enum payment_method: { credit_card: 0, transfer: 1 }
    # カラム名（payment_methodカラムに対して）
-  enum status: { "入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4}
+  enum status: {  "入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4}
 end
